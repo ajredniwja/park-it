615fe6be03906f81df76103b3350158050c1057b
Parminder Singh <psingh35@mail.greenriver.edu>
2018-05-03 17:37:02 -0400

    3rd AND 4th step of pair program done

--- js/park-it.js
+++ js/park-it.js
@@ -1,4 +1,15 @@
-$("#gotIt").click(function()
+function initMap()
 {
-    alert("working");
-});
+    var grc = {lat: 47.3135852, lng:-122.1800072};
+    var DicksDrive = {lat: 47.661062, lng:-122.327777};
+    var mapDiv = new google.maps.Map(document.getElementById('map'),
+        {
+            zoom:11,
+            center: DicksDrive
+        });
+    var marker = new google.maps.Marker(
+        {
+            position: DicksDrive,
+            map: mapDiv
+        });
+}
